99875f517532050f93197753106f81796262439c
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-06 17:27:39 -0700

    changing index style for left partition.

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,42 +1,46 @@
+/*jslint indent:2*/
+'use strict';
+
 function sort(list) {
-	var auxVal,
-    i,
+  var i,
     j,
     key;
 
-    // traverse the array from left to right
-    for (i = 1; i < list.length; i = i+1) {
-      // mark the current position so that
-      // we'll have two partitions
-      // left: [0,i], right: [i+1, list.length]
-      j = i; // element i is what we call the `key`
-      key = list[i];
-      // traverse the left side of the array
-      // from right to left, while there are
-      // elements at the left of the key
-      while (j > 0
-        // and the key is lesser than it's left neighbor
-        && key < list[j-1]){
-        // swap the key and it's left neighbor
-        list[j] = list[j-1];
-        // scan next value to the left
-        j = j-1;
-      }
-      // now the key is at the left of all 
-      // values greater than itself
-      list[j] = key;
+  // traverse the array from left to right
+  // starting with the second element
+  for (i = 1; i < list.length; i = i + 1) {
+    // mark the current position so that
+    // we'll have two partitions
+    // left: [0,i], right: [i+1, list.length]
+    key = list[i];
+
+    // traverse the left partition of the array
+    j = i - 1;
+    // from right to left, while there are
+    // elements at the left of the key
+    while (j >= 0
+        // and the key is lesser the current element
+        && key < list[j]) {
+      // move the current element to the right
+      list[j + 1] = list[j];
+      // scan next value to the left
+      j = j - 1;
     }
-    return list;
+    // the j-th element must be leq than the key
+    // thus place the key next to it
+    list[j + 1] = key;
+  }
+  return list;
 }
 
-var list= [2,4,9,5,3,8,7,5];
+var list = [2, 4, 9, 5, 3, 8, 7, 5];
 console.log('Original:' + list);
 console.log('Sorted:' + sort(list));
 
-list= [72,4,69,5,43,8,7,25];
+list = [72, 4, 69, 5, 43, 8, 7, 25];
 console.log('Original:' + list);
 console.log('Sorted:' + sort(list));
 
-list= [72,4,69,5,43,8,7,25,0,-12,-45,5,7,-2];
+list = [72, 4, 69, 5, 43, 8, 7, 25, 0, -12, -45, 5, 7, -2];
 console.log('Original:' + list);
 console.log('Sorted:' + sort(list));
