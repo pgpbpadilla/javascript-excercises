9c1f4e58f30fb381fa0ba9600d4554af08db2bbe
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-09 21:53:17 -0700

    adding forgotten file (+1 squashed commit) Squashed commits: [03670cc] Only add to the memory array when using memoization, calculate the size of the object when using memorization. (+1 squashed commit) Squashed commits: [a8bb376] Adding memoization.

--- string-combinations.js
+++ string-combinations.js
@@ -2,15 +2,36 @@
 /*jslint indent:2*/
 'use strict';
 
-// find all posible strings that can be formed with 
+// this will be dictionary containing
+// - key: a string
+// - all possible words with the characters of the key
+var memory = {},
+  callsToFunction = 0;
+
+// find all posible strings that can be formed with
 // n characters, without repeating characters
-function findAllWords(string) {
+// @param string {String} The initial String to use to generate all combinations
+// @memoization {bool} Whether or not you want to use memoization
+function findAllWords(string, memoization) {
   var allWords = [],
     i,
     firstChar = '',
     wordsWithSubstring,
     remainingChars,
     j;
+
+  // count the number calls to this function 
+  callsToFunction = callsToFunction + 1;
+  // if we've already seen this word and have
+  // calculated it's combinations, then just
+  // return it
+  if ('true' === memoization // only use memoization when set to true
+      && memory.hasOwnProperty(string)) {
+    // return the array of all the possible words
+    // formed with the chars of `string`
+    return memory[string];
+  }
+
   // if there's only one char in the string
   // there's only one word tha can be formed
   if (1 === string.length) {
@@ -22,10 +43,10 @@ function findAllWords(string) {
   for (i = 0; i < string.length; i = i + 1) {
     // fix the first character
     firstChar = string.charAt(i);
-    // then recursively find all posible words with the 
+    // then recursively find all posible words with the
     // remaining characters
     remainingChars = string.substr(0, i) + string.substr(i + 1);
-    wordsWithSubstring = findAllWords(remainingChars);
+    wordsWithSubstring = findAllWords(remainingChars, memoization);
 
     // add all words that begin with the `firstChar`
     for (j = 0; j < wordsWithSubstring.length; j = j + 1) {
@@ -33,38 +54,40 @@ function findAllWords(string) {
     }
   }
 
+  if ('true' === memoization) {
+    // add all the words to our dictionary
+    memory[string] = allWords;
+  }
   return allWords;
 }
 
 function printWords(stringArray) {
-  var i;
+  var i,
+    words = '';
   for (i = 0; i < stringArray.length; i = i + 1) {
-    console.log(stringArray[i]);
+    words += stringArray[i];
+    if (i < stringArray.length - 1) {
+      words += ',';
+    }
   }
+  console.log(words);
 }
 
 var myWord = 'a',
-  prompt = require('prompt');
+  prompt = require('prompt'),
+  sizeof = require('./sizeof');
 
-printWords(findAllWords(myWord));
-
-myWord = 'ab';
-printWords(findAllWords(myWord));
-
-myWord = 'abc';
-printWords(findAllWords(myWord));
-
-myWord = 'abc';
-printWords(findAllWords(myWord));
-
-console.log('Give me the initial string:');
-prompt.get(['string'], function (err, result) {
+console.log('Setup:');
+prompt.get(['string', 'memoization'], function (err, result) {
   var found;
   if (err) {
     console.log(err);
   } else {
-    found = findAllWords(result.string);
+    found = findAllWords(result.string, result.memoization);
     printWords(found);
     console.log(found.length + ' words found.');
+    console.log('Calls to `findAllWords`:' + callsToFunction);
+    console.log('Memory used: ' + sizeof(memory));
+    callsToFunction = 0;
   }
 });
