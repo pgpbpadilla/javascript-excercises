a52ae4d6fd01dd4ea48d6efb288684f5f6745d91
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-13 00:31:55 -0700

    Adding recursive version using memoization/cache.

--- fibonacci.js
+++ fibonacci.js
@@ -29,13 +29,73 @@ function iterFibonacci(n) {
   return fib;
 }
 
+function recursiveFibonacci(n) {
+
+  var cache = [1, 1],// use memoization
+    fib = [],
+    i = 0,
+    currentFib;
+
+  // return the n-th Fibonacci number
+  function nthFib(nth) {
+    // we've calculated this element before
+    if (cache[nth] && typeof cache[nth] === 'number') {
+      return cache[nth];
+    }
+
+    // base cases, halting conditions
+    if (0 === nth || 1 === nth) {
+      return 1;
+    }
+
+    // Divide et impera
+    currentFib = nthFib(nth - 1) + nthFib(nth - 2);
+    // add element to cache
+    cache.push(currentFib);
+    return currentFib;
+  }
+
+  // add all Fibonacci numbers to an array.
+  for (i = 0; i < n; i = i + 1) {
+    // add the nth Fibonacdi number
+    fib.push(nthFib(i));
+  }
+
+  return fib;
+}
+
 var prompt = require('prompt'),
-  max;
+  max,
+  results,
+  start,
+  end,
+  timeIter = 0,
+  timeRecursive = 0;
 
 console.log('How many Fibonacci numbers?');
 prompt.get(['max'], function (err, result) {
   if (err) {
     console.log(err);
   }
-  console.log(iterFibonacci(result.max));
+  console.log('Iterative:');
+  start = new Date().getTime();
+  results = iterFibonacci(parseInt(result.max, 10));
+  end = new Date().getTime();
+  console.log(results.join(', '));
+  timeIter = end - start;
+
+  results = [];
+  start = 0;
+  end = 0;
+
+  console.log('Recursive:');
+  start = new Date().getTime();
+  results = recursiveFibonacci(parseInt(result.max, 10));
+  end = new Date().getTime();
+  console.log(results.join(', '));
+  timeRecursive = end - start;
+
+  console.log('TimeIter: ' + timeIter);
+  console.log('TimeRecursive: ' + timeRecursive);
+
 });
