b531a6016317d3f72f0b54868893c7d2a36857f9
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-27 10:48:57 -0700

    Exporting to Node and the Browser

--- gists/sizeof/sizeof.js
+++ gists/sizeof/sizeof.js
@@ -1,35 +1,46 @@
-/*jslint indent:2*/
+/*jslint indent:2, browser: true, node: true*/
+
 /** Taken from: http://stackoverflow.com/a/11900218/400544 */
 
 'use strict';
 
-module.exports = function (object) {
+(function () {
+  var typeOfWindow = typeof window;
 
-  var objectList = [],
-    stack = [ object ],
-    bytes = 0,
-    value,
-    i;
+  function sizeof(object) {
+    var objectList = [],
+      stack = [ object ],
+      bytes = 0,
+      value,
+      i;
 
-  while (stack.length) {
-    value = stack.pop();
+    while (stack.length) {
+      value = stack.pop();
 
-    if (typeof value === 'boolean') {
-      bytes += 4;
-    } else if (typeof value === 'string') {
-      bytes += value.length * 2;
-    } else if (typeof value === 'number') {
-      bytes += 8;
-    } else if (typeof value === 'object'
-        && objectList.indexOf(value) === -1) {
-      objectList.push(value);
+      if (typeof value === 'boolean') {
+        bytes += 4;
+      } else if (typeof value === 'string') {
+        bytes += value.length * 2;
+      } else if (typeof value === 'number') {
+        bytes += 8;
+      } else if (typeof value === 'object'
+          && objectList.indexOf(value) === -1) {
+        objectList.push(value);
 
-      for (i in value) {
-        if (value.hasOwnProperty(i)) {
-          stack.push(value[i]);
+        for (i in value) {
+          if (value.hasOwnProperty(i)) {
+            stack.push(value[i]);
+          }
         }
       }
     }
+    return bytes;
   }
-  return bytes;
-};
+
+  // export function
+  if ('undefined' !== typeOfWindow) { // export to window
+    window.sizeof = sizeof;
+  } else { // export to node
+    module.exports = sizeof;
+  }
+}());
