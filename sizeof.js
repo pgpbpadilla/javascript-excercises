fedda5b3a11aecfae9107071db8e4c30b5da62be
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-29 22:02:20 -0700

    Squashed 'gists/sizeof/' changes from 531e618..b5c9ca2
    b5c9ca2 Exporting to Node and the Browser
    01b34db Move to using subtree for sizeof Gist
    
    git-subtree-dir: gists/sizeof
    git-subtree-split: b5c9ca2d90d0b691ad060a058b1b7a15b1e11ebf

--- sizeof.js
+++ sizeof.js
@@ -1,34 +1,46 @@
+/*jslint indent:2, browser: true, node: true*/
+
 /** Taken from: http://stackoverflow.com/a/11900218/400544 */
-function sizeof( object ) {
 
-    var objectList = [];
-    var stack = [ object ];
-    var bytes = 0;
+'use strict';
 
-    while ( stack.length ) {
-        var value = stack.pop();
+(function () {
+  var typeOfWindow = typeof window;
 
-        if ( typeof value === 'boolean' ) {
-            bytes += 4;
-        }
-        else if ( typeof value === 'string' ) {
-            bytes += value.length * 2;
-        }
-        else if ( typeof value === 'number' ) {
-            bytes += 8;
-        }
-        else if
-        (
-            typeof value === 'object'
-            && objectList.indexOf( value ) === -1
-        )
-        {
-            objectList.push( value );
+  function sizeof(object) {
+    var objectList = [],
+      stack = [ object ],
+      bytes = 0,
+      value,
+      i;
 
-            for( var i in value ) {
-                stack.push( value[ i ] );
-            }
+    while (stack.length) {
+      value = stack.pop();
+
+      if (typeof value === 'boolean') {
+        bytes += 4;
+      } else if (typeof value === 'string') {
+        bytes += value.length * 2;
+      } else if (typeof value === 'number') {
+        bytes += 8;
+      } else if (typeof value === 'object'
+          && objectList.indexOf(value) === -1) {
+        objectList.push(value);
+
+        for (i in value) {
+          if (value.hasOwnProperty(i)) {
+            stack.push(value[i]);
+          }
         }
+      }
     }
     return bytes;
-}
+  }
+
+  // export function
+  if ('undefined' !== typeOfWindow) { // export to window
+    window.sizeof = sizeof;
+  } else { // export to node
+    module.exports = sizeof;
+  }
+}());
