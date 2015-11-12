dd5c723b31310b3bb25d54aa8fa77d8b8f56384f
Pablo Padilla <pgpb.padilla@gmail.com>
2015-11-11 22:40:57 -0800

    chg: dev: @wip: implement the rotate method

--- matrix-rotation.js
+++ matrix-rotation.js
@@ -11,27 +11,45 @@ module.exports.MatrixRotator = {
       matrix.push([]);// create new row
 
       for (col = 0; col < size; col = col + 1) {
-        matrix[row].push(Math.ceil(Math.random()*size));
+        matrix[row].push((1+row)*size + col);
       }
 
     }
 
     return matrix;
   },
-  rotate: function (matrix, size) {
+  rotate: function (matrix) {
 
-    function rotateLayer (matrix, layer, layerSize) {
+    // layer is zero-based
+    function rotateLayer (m, layer) {
       
-      
-      
-      return matrix; 
+      var i, tmp, start, size, end;
+
+      start = layer;
+      size = m.length - 2*layer;
+      end = start + size - 1;
+
+      console.log('start', start);
+      console.log('size', size);
+      console.log('end', end);
+
+      for (i = start; i < end; i = i + 1) {
+        console.log('i', i);
+        tmp = m[i][start]; // top
+        m[i][start] = m[start][start + size - i]; // left -> top
+        m[start][start + size - i] = m[end - i][end]; // bottom -> left
+        m[end - i][end] = m[end][i]; // right -> bottom
+        m[end][i] = tmp;// tmp -> right
+      }
     }
     
-    var layers = Math.ceil(size/2), 
+    var layers = Math.ceil(matrix.length/2), 
       l;
+    console.log('layers', layers);
 
-    for (l = layers; l > 0; l = l - 1) {
-      rotateLayer(matrix, l, size - 2*l);
+    for (l = 0; l < layers; l = l + 1) {
+      rotateLayer(matrix, l);
+      console.log('step' + l, matrix);
     }
 
     return matrix;
@@ -53,7 +71,7 @@ module.exports.MatrixRotator = {
 
 // Main program
 var Rotator = exports.MatrixRotator;
-var matrix = Rotator.randomMatrix(5);
+var matrix = Rotator.randomMatrix(3);
 
 
 console.log('Random matrix:\n' + Rotator.toString(matrix));
