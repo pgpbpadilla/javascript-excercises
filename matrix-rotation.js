6fd8d8318c046924bef15e84980e88909b4c69e6
Pablo Padilla <pgpb.padilla@gmail.com>
2015-11-15 01:41:18 -0800

    chg: dev: Add markers to the elements just modified on each step when in log level: 'debug'

--- matrix-rotation.js
+++ matrix-rotation.js
@@ -3,21 +3,70 @@
 
 'use strict';
 
-module.exports.MatrixRotator = {
-  randomMatrix: function (size) {
-    var matrix = [], row, col;
+(function () {
+  
+  'use strict';
 
-    for (row = 0; row < size; row = row + 1) {
-      matrix.push([]);// create new row
+  var winston = require('winston');
 
-      for (col = 0; col < size; col = col + 1) {
-        matrix[row].push((1+row)*size + col);
+  var logger = new (winston.Logger)({
+    level: 'info',
+    transports: [
+      new (winston.transports.Console)()
+    ]
+  });
+
+  function shouldMarkEntryAt(row, col, entries) {
+    var result, i;
+
+    for(i = 0; i < entries.length; i = i + 1) {
+      if (row === entries[i].row 
+          && col === entries[i].col) {
+        return true;
       }
-
     }
 
-    return matrix;
-  },
+    return false;
+  }
+
+  function formatMatrix(matrix, markedEntries) {
+      var result = '';
+
+      matrix.forEach(function (row, rowIdx) {
+        row.forEach(function (value, colIdx) {
+
+          if (undefined !== markedEntries 
+              && shouldMarkEntryAt(rowIdx, colIdx, markedEntries)) {
+            result += '>' + value + '<';
+          } else {
+            result += value;
+          }
+
+          result += '\t';
+
+        });
+
+        result += '\n';
+      });
+
+      return result;
+    }
+
+  module.exports.MatrixRotator = {
+    randomMatrix: function (size) {
+      var matrix = [], row, col;
+
+      for (row = 0; row < size; row = row + 1) {
+        matrix.push([]);// create new row
+
+        for (col = 0; col < size; col = col + 1) {
+          matrix[row].push((1+row)*size + col);
+        }
+
+      }
+
+      return matrix;
+    },
     copyMatrix: function (m) {
       var copy = [];
 
@@ -30,56 +79,87 @@ module.exports.MatrixRotator = {
 
       return copy;
     },
-  rotate: function (matrix) {
+    rotate: function (matrix) {
 
-    // layer is zero-based
-    function rotateLayer (m, layer) {
-      
+      // layer is zero-based
+      function rotateLayer (m, layer) {
+        
         var step, tmp, start, size, end, maxSteps;
 
-      start = layer;
-      end = m.length - 1 - layer;
+        start = layer;
+        end = m.length - 1 - layer;
         maxSteps = end - start;
 
+        logger.debug('START', start);
+        logger.debug('END', end);
 
         for (step = 0; step < maxSteps; step = step + 1) {
+          logger.debug('STEP - ', step);
           
           tmp = m[start][start + step]; // tmp <- top
 
           m[start][start + step] = m[end - step][start]; 
+          logger.debug('MOVED top <- left\n', formatMatrix(matrix, [
+            {
+              row: end - step, 
+              col: start
+            },
+            {
+              row: start, 
+              col: start + step
+            }
+          ]));
 
           m[end - step][start] = m[end][end - step]; 
+          logger.debug('MOVED left <- bottom\n', formatMatrix(matrix, [
+            {
+              row: end,
+              col: end - step
+            },
+            {
+              row: end - step, 
+              col: start
+            }
+          ]));
 
           m[end][end - step] = m[start + step][end]; 
+          logger.log('MOVED bottom <- right\n', formatMatrix(matrix, [
+            {
+              row: start + step,
+              col: end
+            },
+            { 
+              row: end,
+              end: end - step
+            }
+          ]));
 
           m[start + step][end] = tmp;
+          logger.debug('MOVED right <- top[tmp]\n', formatMatrix(matrix, [
+            {
+              row: start + step,
+              col: end
+            }
+          ]));
+        }
       }
-    }
-    
-    var layers = Math.ceil(matrix.length/2), 
-      l;
-    console.log('layers', layers);
+      
+      var layers = Math.ceil(matrix.length/2), 
+          l;
+      logger.debug('LAYERS', layers);
 
-    for (l = 0; l < layers; l = l + 1) {
-      rotateLayer(matrix, l);
-      console.log('step' + l, matrix);
-    }
+      for (l = 0; l < layers; l = l + 1) {
+        logger.debug('ROTATING LAYER - ' + l + '\n');
+        rotateLayer(matrix, l);
+      }
 
-    return matrix;
-  },
-  toString: function (matrix) {
-    var result = '';
+      return matrix;
+    },
+    toString: formatMatrix
+  };
+  
+}());
 
-    matrix.forEach(function (row) {
-      row.forEach(function (value) {
-        result += value + '\t';
-      });
-      result += '\n';
-    });
-
-    return result;
-  }
-};
 
 
 // Main program
